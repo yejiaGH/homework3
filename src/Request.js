4b8f44010da8d083ebe670064763ff6cb907ee1b
yejia <ye.jia.alice@gmail.com>
2017-08-22 01:12:32 +0800

    draft 1

---
+++ src/Request.js
@@ -0,0 +1,16 @@
+const DB = require('../lib/db.js')
+
+class Request extends DB () {
+    constructor(options={}) {
+        super(options)
+    }
+
+    request() {
+        super.request()
+        return this
+    }
+
+    get() {
+        console.log('get')
+    }
+}
